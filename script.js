0d902b54d239532b90ea02dc4c550260758c6203
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-08 08:34:08 +0100

    Created early elements of library program

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function Book() {
+let myLibrary = [];
+addBookToLibrary('Book1', 'Author1', 298, true);
+addBookToLibrary('Book2', 'Author2', 172, false);
+addBookToLibrary('Book3', 'Author3', 512, true);
+addBookToLibrary('Book4', 'Author4', 467, true);
+
+function Book(title, author, pages, read) {
     this.title = title
     this.author = author
-    this.numPages = numPages
-    this.read = Boolean(read)
-    this.info = function() {
-        return Object.values(this);
-    }
-    console.log(theHobbit.info());
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary(title, author, pages,  read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
+
+function displayLibrary() {
+    console.table(myLibrary)
 }
