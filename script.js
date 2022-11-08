da0e531c6682dc6e1705f46400e22377c6064901
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-11-08 10:48:33 +0100

    Finished first parts of library and styling, input is placeholder

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 let myLibrary = [];
+
+function Book(Title, Author, Pages, Read) {
+    this.Title = Title
+    this.Author = Author
+    this.Pages = Pages
+    this.Read = rRad
+}
+
+function addBookToLibrary(Title, Author, Pages,  Read) {
+    let book = new Book(Title, Author, Pages, Read);
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    const books = document.querySelector(".books")
+    myLibrary.forEach(myLibrary => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        books.appendChild(card);
+        for (let key in myLibrary) {
+            console.log(`${key}: ${myLibrary[key]}`);
+            const para = document.createElement("p");
+            para.textContent(`${key}: ${myLibrary[key]}`);
+            card.appendChild(para); 
+        }
+    })
+}
 addBookToLibrary('Book1', 'Author1', 298, true);
 addBookToLibrary('Book2', 'Author2', 172, false);
 addBookToLibrary('Book3', 'Author3', 512, true);
 addBookToLibrary('Book4', 'Author4', 467, true);
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
-
-function addBookToLibrary(title, author, pages,  read) {
-    myLibrary.push(new Book(title, author, pages, read));
-}
-
-function displayLibrary() {
-    console.table(myLibrary)
-}
+console.log("End of array", myLibrary);
+displayBooks();
